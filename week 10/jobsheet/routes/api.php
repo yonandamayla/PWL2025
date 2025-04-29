4a12956f8c9efcbefd3e0c5e461cb52e2af78dd6
yonandamayla <yonandamayla555@gmail.com>
2025-04-29 08:44:13 +0700

    feat(api): implement kategori CRUD operations via RESTful API

--- week 10/jobsheet/routes/api.php
+++ week 10/jobsheet/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LevelController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,4 +40,13 @@ Route::middleware('auth:api')->group(function() {
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
+
+// kategori
+Route::middleware('auth:api')->group(function() {
+    Route::get('/kategori', [KategoriController::class, 'index']);
+    Route::post('/kategori', [KategoriController::class, 'store']);
+    Route::get('/kategori/{id}', [KategoriController::class, 'show']);
+    Route::put('/kategori/{id}', [KategoriController::class, 'update']);
+    Route::delete('/kategori/{id}', [KategoriController::class, 'destroy']);
 });
